53128d6ee7fe564898004256a53055f10ccceb59
ekathuria <e@ekathuria.dev>
2022-04-09 12:35:48 -0400

    Set up test functionality

--- server/controllers.js
+++ server/controllers.js
@@ -1,5 +1,27 @@
 // create user
 
+
 // get user
 
 // update user
+
+const Test = {};
+
+Test.getSong = (req, res, next) => {
+  fetch("https://api.spotify.com/v1/audio-analysis/6EJiVf7U0p1BBfs0qqeb1f", {
+    method: "GET",
+    headers: {
+      // Authorization: `Bearer ${userAccessToken}`
+      Authorization: `Bearer ${process.env.CLIENT_ID}`
+    }
+  })
+  .then(response => response.json())
+  .then(({beats}) => {
+    beats.forEach((beat, index) => {
+      console.log(`Beat ${index} starts at ${beat.start}`);
+    })
+    return next();
+  })
+}
+
+module.exports = Test;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+const Test = require('./controllers')
 const express = require('express');
 // const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use('/', Test, (req, res) => {
+  console.log('data received');
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log('Server listening on port: ' + PORT);
