386b15839e58a6a270a27bedd57b3d4df83558c7
Alex Adams <alexadams@Alexs-MacBook.local>
2022-04-11 11:43:51 -0400

    implemented spotify search api into React with axios

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,33 +1,69 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-// const SpotifyWebApi = require('spotify-web-api-js');
+import axios from 'axios';
+import qs from 'qs';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 class Form extends Component {
     constructor(props) {
       super(props);
-      
+      this.spotifyApi = new SpotifyWebApi();
+      // TODO:
+      // change from hardcode to process.env variables sent from backend
+      this.clientId = '65f25198a7e44731924a5639662a68b6';
+      this.clientSecret = 'ca0c2f2976ae40388596603a0b1d92eb';
+  
+      this.artist = 'Polyphia';
+      this.track = '40oz';
     }
 
-    // async handleQuerySubmit() {
-    //   const token = await axios();
-    //   const spotifyApi = new SpotifyWebApi({
-    //     clientId: '65f25198a7e44731924a5639662a68b6', // Alex's app client ID
-    //     clientSecret: 'ca0c2f2976ae40388596603a0b1d92eb', // Alex's app client secret
-    //   });
-    //   console.log('Submit was pressed.')
-    //   spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
-    //     function(data) {
-    //       console.log('Artist albums', data.body);
-    //     },
-    //     function(err) {
-    //       console.error(err);
-    //     }
-    //   );
-    // }
+    getAccessToken = async () => {
+      try {
+        const tokenUrl = 'https://accounts.spotify.com/api/token';
+        const data = qs.stringify(
+          {
+            grant_type: 'client_credentials',
+            client_id: this.clientId,
+            client_secret: this.clientSecret,
+          },
+        );
+        const response = await axios.post(tokenUrl, data, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        });
+        return response.data.access_token;
+      } catch (error) {
+        console.log(error);
+        return error;
+      }
+    };
+
+    queryTracks = async (queryString) => {
+      const accessToken = await this.getAccessToken();
+      this.spotifyApi.setAccessToken(accessToken);
+      const allResults = await this.spotifyApi.searchTracks(queryString)
+        .then((data) => {
+          console.log(data);
+          return data.tracks.items
+        });
+      console.log(allResults);
+      console.log('--------ALL RESULTS--------\n', allResults);
+      const topResult = allResults[0];
+      console.log('--------TOP RESULT--------\n', topResult);
+      const { album, artists, name } = topResult;
+      console.log('--------SONG NAME--------\n', name);
+      console.log('--------ALBUM--------\n', album);
+      console.log('--------ARTIST(S)--------\n', artists);
+    };
+
+    async handleQuerySubmit(event) {
+      event.preventDefault();
+      await this.queryTracks(`track:${this.track} artist:${this.artist}`)
+    }
 
     render() {
      return(
-        <form id="search-form" onSubmit={() => console.log('Just submitted a query')}>
+        <form id="search-form" onSubmit={(event) => this.handleQuerySubmit(event)}>
           <p>
             Type an artist, album, or song to start your search.
           </p>
@@ -39,5 +75,5 @@ class Form extends Component {
       )
     }
   }
-  
+
   export default Form;
